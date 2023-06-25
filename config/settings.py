87a4e2e0aa966607ed2d8722ff0b68eadb514026
Doston <dostonlv@icloud.com>
2023-06-25 10:44:00 +0500

    heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-87*e2kf6d-m@7akpmn_h+dlh==65hb%fi**to8)*e7uqmoxfx8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
